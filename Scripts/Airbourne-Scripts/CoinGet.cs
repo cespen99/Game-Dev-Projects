7da0248376298b6f5a3a93af97f160f3c8bd4485
unknown <fvk589@my.utsa.edu>
2022-03-02 11:31:47 -0600

    second commit

---
+++ Scripts/Airbourne-Scripts/CoinGet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinGet : MonoBehaviour {
+
+    public float value;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+		
+	}
+}
---
+++ Scripts/Airbourne-Scripts/ObjectMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour {
+
+    public float objectSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(new Vector3(-1 * objectSpeed, 0f, 0f));
+		
+	}
+}
